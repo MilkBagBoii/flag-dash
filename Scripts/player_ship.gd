1bcf70838ddb8b32a565662484af1d04e1703ad5
MilkBagBoii <johnleonardo207@gmail.com>
2025-11-09 16:37:44 -0500

    ship redundent

--- Scripts/player_ship.gd
+++ Scripts/player_ship.gd
@@ -9,9 +9,9 @@ func _physics_process(delta: float) -> void:
 	
 	if (Input.is_action_pressed("ui_accept")):
 		pass
+	
 	if (Input.is_action_pressed("ui_right")):
 		apply_torque(Vector3.UP * -turn_speed)
 	elif  (Input.is_action_pressed("ui_left")):
 		apply_torque(Vector3.UP * turn_speed)
 	super._physics_process(delta)
-	
