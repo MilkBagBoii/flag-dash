52db5b879d5f84458e4d106e9ca54715ed1cf609
MilkBagBoii <johnleonardo207@gmail.com>
2025-11-12 21:27:15 -0500

    ship fix

--- Scripts/smoothboat.gd
+++ Scripts/smoothboat.gd
@@ -2,7 +2,8 @@ extends Node3D
 
 @export var interp_speed: float = 50
 @onready var top: Node3D = $".."
-
+func _ready() -> void:
+	top_level = true
 func _process(delta: float) -> void:
 	var flat_pos = Vector3(top.global_position.x, global_position.y, top.global_position.z)
 	global_position = lerp(global_position, flat_pos, interp_speed * delta / 2)
